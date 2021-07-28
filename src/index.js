6d31c277617ed42a5b2b09b234aa78537156cdf6
Maksym Dzhafarov <maksim8797@gmail.com>
2021-07-28 12:22:11 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
-// import '../node_modules/simplelightbox/dist/simple-lightbox.min.css';
+import '../node_modules/simplelightbox/dist/simple-lightbox.min.css';
 import './sass/main.scss';
 import './js/main';
